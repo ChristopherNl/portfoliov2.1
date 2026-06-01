3b5527ef0054874092b51aff1ae44fd7bc506d8d
MR.K <neozya22@gmail.com>
2026-06-01 17:50:38 +0200

    work on content

--- script.js
+++ script.js
@@ -34,3 +34,25 @@ if (backToTop) {
     });
   });
 }
+
+const introScreen = document.getElementById("introScreen");
+const unlockBtn = document.getElementById("unlockBtn");
+const langBtns = document.querySelectorAll(".lang-btn");
+
+unlockBtn.addEventListener("click", () => {
+  introScreen.style.opacity = "0";
+  introScreen.style.pointerEvents = "none";
+
+  setTimeout(() => {
+    introScreen.style.display = "top";
+  }, 500);
+});
+
+langBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    langBtns.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+    const lang = btn.dataset.lang;
+    document.documentElement.lang = lang;
+  });
+});
